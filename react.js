b08f3bb39844f084657bb33c9762a182b02f9d39
Justsnoopy30 <everettallen30@gmail.com>
2020-03-06 20:44:10 -0600

    Change way to load react and react-dom.

---
+++ react.js
@@ -0,0 +1,3 @@
+import { React, ReactDOM } from 'https://unpkg.com/es-react/dev/index.js';
+import ReactApp from './react-app.js';
+ReactDOM.render(React.createElement(ReactApp, null), document.getElementById("root"));
