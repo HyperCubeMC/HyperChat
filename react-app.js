6d890a075156800cc19e9d28584b60302c18ea68
Justsnoopy30 <everettallen30@gmail.com>
2020-03-24 12:58:18 -0500

    Comment out Hello World react rendering.

--- react-app.js
+++ react-app.js
@@ -1,13 +1,13 @@
-import { React, ReactDOM, Component } from 'https://unpkg.com/es-react/dev/index.js';
-
-class ReactApp extends Component{
-  render(){
-    return(
-      React.createElement("div", {
-        className: "ReactApp"
-      }, React.createElement("h1", null, " Hello, World! "))
-    );
-  }
-}
-
-export default ReactApp;
+// import { React, ReactDOM, Component } from 'https://unpkg.com/es-react/dev/index.js';
+//
+// class ReactApp extends Component{
+//   render(){
+//     return(
+//       React.createElement("div", {
+//         className: "ReactApp"
+//       }, React.createElement("h1", null, " Hello, World! "))
+//     );
+//   }
+// }
+// 
+// export default ReactApp;
