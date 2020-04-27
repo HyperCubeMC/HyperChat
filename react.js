82320442ad7901d79b43186c14c531eeee389c36
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2020-04-27 16:29:19 -0500

    Update jQuery to 3.5.0 and remove unused libraries (react and jQuery UI).

--- react.js
+++
@@ -1,3 +0,0 @@
-import { React, ReactDOM } from 'https://unpkg.com/es-react/dev/index.js';
-import ReactApp from './react-app.js';
-ReactDOM.render(React.createElement(ReactApp, null), document.getElementById("root"));
